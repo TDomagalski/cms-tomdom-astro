149edf7165a16dd2ba8c499eb79000971a381672
TomDom <tomekwebdev@gmail.com>
2025-09-05 19:34:28 +0200

    fix: fix formating .astro files with this: require.resolve('prettier-plugin-astro')

---
+++ prettier.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  plugins: [require.resolve("prettier-plugin-astro")],
+  overrides: [
+    {
+      files: "*.astro",
+      options: { parser: "astro" },
+    },
+  ],
+};
